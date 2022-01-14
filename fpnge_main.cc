a26d1a38b11d91cbb4b5e5c1c20ee8cf25afc4a9
Pascal Massimino <skal@google.com>
2022-01-14 16:50:40 +0100

    fix typo in help msg

--- fpnge_main.cc
+++ fpnge_main.cc
@@ -24,7 +24,7 @@
 
 int main(int argc, char **argv) {
   if (argc < 3) {
-    fprintf(stderr, "Usage: %s in.png out.jxl [num_reps]\n", argv[0]);
+    fprintf(stderr, "Usage: %s in.png out.png [num_reps]\n", argv[0]);
     return 1;
   }
 
