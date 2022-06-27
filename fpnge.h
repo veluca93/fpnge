13f200d9c890443b576ea0d27b58b058a86cf5a7
animetosho <animetosho@users.noreply.github.com>
2022-06-27 15:40:16 +1000

    Prepare extern C for future additions

--- fpnge.h
+++ fpnge.h
@@ -16,8 +16,9 @@
 #include <stdlib.h>
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
+
 // bytes_per_channel = 1/2 for 8-bit and 16-bit. num_channels: 1/2/3/4
 // (G/GA/RGB/RGBA)
 size_t FPNGEEncode(size_t bytes_per_channel, size_t num_channels,
@@ -28,4 +29,9 @@ inline size_t FPNGEOutputAllocSize(size_t bytes_per_channel, size_t num_channels
   // likely an overestimate
   return 1024 + 2 * bytes_per_channel * num_channels * width * height;
 }
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
